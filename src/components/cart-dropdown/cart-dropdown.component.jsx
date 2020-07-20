9a553810866b1f384a1b8f47bda460feba2fa633
baldomar <hack0815@notna.me>
2020-07-20 21:19:34 +0200

    added quantity to cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,9 +6,10 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => (
   <div className='cart-dropdown'>
-    <div className='cart-items'/>
+    <div className='cart-items' />
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
+
 
 export default CartDropdown;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import SHOP_DATA from "./shop.data.js";
 import CollectionPreview from "./../../components/preview-collection/preview-collection.component";
+import './shop.styles.scss';
 
 class ShopPage extends React.Component {
   constructor(props) {
